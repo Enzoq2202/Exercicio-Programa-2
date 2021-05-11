7eed4dc9ad8d6ea9a47423c184b40b355e2de2c1
Jpnatal1 <joaopan@al.insper.edu.br>
2021-05-11 17:04:07 -0300

    .

--- main.py
+++ main.py
@@ -2,9 +2,28 @@
 from funcoes import*
 
 
+print('Paciência Acordeão ')
+print('================== ')
+print('')
+print('Seja bem-vindo(a) ao jogo de Paciência Acordeão! O objetivo deste jogo é colocar todas as cartas em uma mesma pilha. ')
+print('')
+print('Existem apenas dois movimentos possíveis: ')
+print('')
+print('1. Empilhar uma carta sobre a carta imediatamente anterior; ')
+print('2. Empilhar uma carta sobre a terceira carta anterior. ')
+print('')
+print('Para que um movimento possa ser realizado basta que uma das duas condições abaixo seja atendida: ')
+print('')
+print('1. As duas cartas possuem o mesmo valor ou ')
+print('2. As duas cartas possuem o mesmo naipe. ')
+print('')
+print('Desde que alguma das condições acima seja satisfeita, qualquer carta pode ser movimentada. ')
+print('')
+input('Aperte [Enter] para iniciar o jogo...')
+
 baralho=cria_baralho()
 baralho=list(set(baralho))
-imprime_baralho(baralho)
-for carta in baralho:
-    int(input('digite a posição da carta que deseja mover:{1} a {len(cartas)}'.format(cartas)))
+while possui_movimentos_possiveis(baralho):
+    imprime_baralho(baralho)
+    a=int(input('digite a posição da carta que deseja mover:{1} a {len(baralho)}'.format(baralho)))
 
